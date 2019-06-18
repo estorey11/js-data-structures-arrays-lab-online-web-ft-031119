1701486255280b2ceb27d4248e7f32764eec65a6
Eric Storey <estorey@gmail.com>
2019-06-18 17:23:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ let drivers=["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name){
   drivers.push(name)
 }
+
+function destructivelyPrependDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
